ccd5a5f5c286762ae509196d94888a2469b089d8
Anton Grigoryev <ipoyzn@gmail.com>
2024-02-11 22:01:02 +0000

    Adds Devise with default config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,4 +71,15 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Ensure you have defined default url options in your environments files. Here
+  # is an example of default_url_options appropriate for a development environment
+  # in config/environments/development.rb:
+  #
+  #   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #
+  # In production, :host should be set to the actual host of your application.
+  #
+  #   * Required for all applications. *
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
